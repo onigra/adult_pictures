f3cede3f35a936fb853d52c33c7cd17ce4924532
onigra <3280467rec@gmail.com>
2012-12-14 13:06:44 +0900

    firstcommit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source :rubygems
+gem 'sinatra'
+gem 'sequel'
+gem 'mysql2'
+gem 'instagram'
+gem 'rake'
+gem 'heroku'
+
---
+++ batch/get_image_url.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'instagram'
+require 'sequel'
+require 'mysql2'
+
+DB = Sequel.connect('mysql2://root:naruhaya@localhost/sinaapp')
+pic = DB[:pictures]
+
+Instagram.configure do |config|
+  config.client_id = "2d6c74c7d3ef427b90c9806eff8dc33e"
+  config.client_secret = "b67ecb7c3d154c47907e03ae2f862976"
+end
+
+client = Instagram.media_popular
+client.map{|item|
+  pic.insert(:url => "#{item.images.standard_resolution.url}", :created_at => Time.now, :updated_at => Time.now)
+}
+
