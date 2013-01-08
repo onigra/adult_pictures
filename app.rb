d4147a5be84415046fc7fe1ca7fb0a1f3c3a7457
onigra <3280467rec@gmail.com>
2013-01-09 00:11:09 +0900

    sqliteからデータをとってくるように修正

--- app.rb
+++ app.rb
@@ -5,18 +5,20 @@ require 'sequel'
 enable :sessions
 set :public_folder, File.dirname(__FILE__) + '/static'
 
-DB = Sequel.connect('sqlite://sinaapp.db')
+DB = Sequel.connect('sqlite://db/sinaapp.db')
 
 pic = Array.new
 50.times {
-  pic << DB[:pictures].filter(:name => (1..15).to_a.sample)
+  DB[:pictures].filter(:id => (1..32).to_a.sample).map{|item|
+    pic << item[:url]
+  }
 }
 
 get "/" do
   @pictures = Array.new
 
-  pic.map{|item|
-     @pictures << "<a href='#{item[:url]}' rel='lightbox'><img src='#{item[:url]}'></img></a>"
+  pic.each{|item|
+    @pictures << "<a href='#{item}' rel='lightbox'><img src='#{item}'></img></a>"
   }
   
   erb :index
