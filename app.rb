3f5694634853706864fd39f91bae7d335f422af2
onigra <3280467rec@gmail.com>
2013-01-09 12:48:49 +0900

    Grid-A-Liciousに変更

--- app.rb
+++ app.rb
@@ -5,24 +5,19 @@ require_relative 'lib/methods'
 
 enable :sessions
 set :public_folder, File.dirname(__FILE__) + '/static'
-
+ 
 DB = Sequel.connect('sqlite://db/sinaapp.db')
-
-pic = Array.new
-select_num_list = create_uniq_num_list
-
-select_num_list.each {|key|
-  DB[:pictures].filter(:id => key).map {|item|
-    pic << item[:url]
-  }
-}
 
 get "/" do
   @pictures = Array.new
-
-  pic.each {|item|
-    @pictures << "<a href='#{item}' rel='lightbox'><img src='#{item}'></img></a>"
-  }
+  select_num_list = create_uniq_num_list
   
+  select_num_list.each {|key|
+    DB[:pictures].filter(:id => key).map {|item|
+      @pictures << "<div class='item'><img src='#{item[:url]}'></img></div>"
+    }
+  }
+
   erb :index
 end
+
--- lib/methods.rb
+++ lib/methods.rb
@@ -4,7 +4,7 @@
 # ランダムで選択して配列にして返すメソッド
 def create_uniq_num_list
   uniq_nums = Array.new
-  nums_list = (1..96).to_a
+  nums_list = (1..160).to_a
   
   (0..49).each {|i|
     uniq_nums << nums_list.sample
