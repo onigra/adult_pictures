fbe80a38e2e21cb4183a36313b1ed48cf717da50
onigra <3280467rec@gmail.com>
2013-01-17 00:46:36 +0900

    変数名修正、なんとなくmapからeachへ変更

--- batch/insert_image_url.rb
+++ batch/insert_image_url.rb
@@ -3,17 +3,17 @@ require 'sequel'
 require 'yaml'
 
 YML = YAML.load_file "../config/instagram.yml"
+DB = Sequel.connect('sqlite://../db/sinaapp.db')
 
 Instagram.configure do |config|
   config.client_id = YML['info']['client_id'] 
   config.client_secret = YML['info']['client_secret'] 
 end
 
-DB = Sequel.connect('sqlite://../db/sinaapp.db')
-pic = DB[:pictures]
+target = DB[:pictures]
+insert_pictures = Instagram.media_popular
 
-client = Instagram.media_popular
-client.map{|item|
-  pic.insert(:url => "#{item.images.standard_resolution.url}", :created_at => Time.now, :updated_at => Time.now)
+insert_pictures.each{|item|
+  target.insert(:url => item.images.standard_resolution.url, :created_at => Time.now, :updated_at => Time.now)
 }
 
