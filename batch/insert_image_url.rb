3e3f3f91833cd6f5335baf04ec1f93e86fe96014
onigra <3280467rec@gmail.com>
2013-01-16 20:12:08 +0900

    接続情報をconfigからとってくるように修正

--- batch/insert_image_url.rb
+++ batch/insert_image_url.rb
@@ -1,13 +1,16 @@
 require 'instagram'
 require 'sequel'
+require 'yaml'
+
+YML = YAML.load_file "../config/instagram.yml"
+
+Instagram.configure do |config|
+  config.client_id = YML['info']['client_id'] 
+  config.client_secret = YML['info']['client_secret'] 
+end
 
 DB = Sequel.connect('sqlite://../db/sinaapp.db')
 pic = DB[:pictures]
-
-Instagram.configure do |config|
-  config.client_id = "2d6c74c7d3ef427b90c9806eff8dc33e"
-  config.client_secret = "b67ecb7c3d154c47907e03ae2f862976"
-end
 
 client = Instagram.media_popular
 client.map{|item|
