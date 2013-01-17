2d69f8517c8e9082ddec36c94e7ff245916414fa
onigra <3280467rec@gmail.com>
2013-01-18 01:13:46 +0900

    レコード更新バッチを論理削除されたものから優先的に更新するように修正

--- batch/update_image_url.rb
+++ batch/update_image_url.rb
@@ -13,10 +13,24 @@ end
 target = DB[:pictures]
 update_pictures = Instagram.media_popular
 
-update_pictures.each{|item|
-  target_id = target.order(:updated_at.asc).limit(1).all[0][:id]
+# リンク切れで削除したレコードを取得
+deleted_pictures = DB["select * from pictures where deleted_at is not null"].all
 
-  target.where('id = ?', target_id)
-        .update(:url => item.images.standard_resolution.url, :updated_at => Time.now)
+update_pictures.each{|item|
+
+  # 削除したレコードが存在すれば、削除したレコードから更新する。
+  # 削除したレコードが無ければ、更新日が古いものから更新していく。
+  if deleted_pictures.empty? || deleted_pictures.nil?
+    target_id = target.order(:updated_at.asc).limit(1).all[0][:id]
+
+    target.where('id = ?', target_id)
+          .update(:url => item.images.standard_resolution.url, :updated_at => Time.now)
+  else
+    target_id = deleted_pictures.shift[:id]
+
+    target.where('id = ?', target_id)
+          .update(:url => item.images.standard_resolution.url, :updated_at => Time.now, :deleted_at => nil)
+  end
+
 }
 
