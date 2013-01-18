f49193042d94c9a52e83e58fb3c2f2bebe07ebc4
onigra <3280467rec@gmail.com>
2013-01-18 21:13:17 +0900

    画像の取得IDリストを500件に固定。heightのリストを増やした。

--- app.rb
+++ app.rb
@@ -10,11 +10,12 @@ DB = Sequel.connect('sqlite://db/sinaapp.db')
 
 get "/" do
   @pictures = Array.new
-  select_num_list = Utils.create_uniq_num_list((1..500).to_a)
-  foo = Utils.random_size
-  
+  select_num_list = Utils.create_uniq_num_list
+  height_list = Utils.create_picture_height
+
   select_num_list.each {|key|
-    size = foo.sample
+    size = height_list.sample
+
     DB[:adult_pictures].filter(:id => key).map {|item|
       @pictures << "<div class='item'><a href='#{item[:url]}' class='gallery'><img src='#{item[:url]}' width=196 height=#{size} ></a></div>"
     }
--- lib/util.rb
+++ lib/util.rb
@@ -5,12 +5,13 @@ class Utils
   # -------------------------------------------------
   #  1から500までの数値の中で、50個のユニークな数値を
   #  ランダムで選択して配列にして返すメソッド
-  #  nums_list = Array
   # -------------------------------------------------
-  def self.create_uniq_num_list(nums_list)
+  def self.create_uniq_num_list
     uniq_nums = Array.new
-    
+
     (0..47).each {|i|
+      nums_list = (1..500).to_a
+
       uniq_nums << nums_list.sample
       nums_list.reject! {|x| x == uniq_nums[i] }
     }
@@ -18,13 +19,16 @@ class Utils
     return uniq_nums
   end
   
-  def self.random_size
-    list = [
+  def self.create_picture_height
+    size_list = [
       196,
       96,
+      86,
+      226,
+      156
     ]
     
-    return list
+    return size_list
   end
 end
 
