78669083dd751ef05ec71dcdd79163195c2ee70a
onigra <3280467rec@gmail.com>
2013-01-18 16:28:26 +0900

    データソース追加

--- app.rb
+++ app.rb
@@ -10,12 +10,12 @@ DB = Sequel.connect('sqlite://db/sinaapp.db')
 
 get "/" do
   @pictures = Array.new
-  select_num_list = Utils.create_uniq_num_list((1..509).to_a)
+  select_num_list = Utils.create_uniq_num_list((1..514).to_a)
   foo = Utils.random_size
   
   select_num_list.each {|key|
     size = foo.sample
-    DB[:pictures].filter(:id => key).map {|item|
+    DB[:adult_pictures].filter(:id => key).map {|item|
       @pictures << "<div class='item'><img src='#{item[:url]}' width=196 height=#{size} ></img></div>"
     }
   }
